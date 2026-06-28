beb9675bb05aba92806115df4136942bbb43fda9
Quang <buidangquang1810@gmail.com>
2026-06-28 16:34:47 +0700

    sửa lỗi unit test admin

--- Backend/lxrhotel.API.Tests/AdminControllerTests.cs
+++ Backend/lxrhotel.API.Tests/AdminControllerTests.cs
@@ -29,7 +29,7 @@ namespace lxrhotel.API.Tests
         {
             // Arrange - Chuẩn bị dữ liệu
             var context = CreateContext();
-            var phong = new Phong { MaPhong = "P101", TrangThai = "Trống" };
+            var phong = new Phong { MaPhong = "P101", TrangThai = "Trống", LoaiPhong = "Standard", MaKs = "KS01" };
             context.Phongs.Add(phong);
             await context.SaveChangesAsync();
 
@@ -64,7 +64,7 @@ namespace lxrhotel.API.Tests
         {
             // Arrange
             var context = CreateContext();
-            var phong = new Phong { MaPhong = "P101", TrangThai = "Trống" };
+            var phong = new Phong { MaPhong = "P101", TrangThai = "Trống", LoaiPhong = "Standard", MaKs = "KS01" };
             var datPhong = new DatPhong
             {
                 MaDatPhong = 1,
@@ -84,7 +84,7 @@ namespace lxrhotel.API.Tests
 
             // Assert
             result.Should().BeOfType<BadRequestObjectResult>()
-                   .Which.Value.Should().BeEquivalentTo(new { message = "Không thể thay đổi trạng thái. Phòng này hiện đang có khách ở." });
+                   .Which.Value.Should().Be("Không thể thay đổi trạng thái. Phòng này hiện đang có khách ở.");
         }
 
         #endregion
@@ -99,21 +99,27 @@ namespace lxrhotel.API.Tests
             var thang = 5;
             var nam = 2024;
 
+            var phong1 = new Phong { MaPhong = "P101", LoaiPhong = "Standard", MaKs = "KS01", TrangThai = "Trống" };
+            var phong2 = new Phong { MaPhong = "P102", LoaiPhong = "Standard", MaKs = "KS01", TrangThai = "Trống" };
+            var phong3 = new Phong { MaPhong = "P103", LoaiPhong = "Standard", MaKs = "KS01", TrangThai = "Trống" };
+            var phong4 = new Phong { MaPhong = "P104", LoaiPhong = "Standard", MaKs = "KS01", TrangThai = "Trống" };
+
             // Đơn thành công trong kỳ
-            var dp1 = new DatPhong { MaDatPhong = 1, TrangThai = "Success", TongTien = 1000 };
+            var dp1 = new DatPhong { MaDatPhong = 1, MaPhong = "P101", TrangThai = "Success", TongTien = 1000 };
             var dc1 = new DatCoc { MaDatCoc = 1, MaDatPhong = 1, NgayDatCoc = new DateTime(nam, thang, 10) };
 
-            var dp2 = new DatPhong { MaDatPhong = 2, TrangThai = "Success", TongTien = 2500 };
+            var dp2 = new DatPhong { MaDatPhong = 2, MaPhong = "P102", TrangThai = "Success", TongTien = 2500 };
             var dc2 = new DatCoc { MaDatCoc = 2, MaDatPhong = 2, NgayDatCoc = new DateTime(nam, thang, 15) };
 
             // Đơn đang chờ trong kỳ
-            var dp3 = new DatPhong { MaDatPhong = 3, TrangThai = "Pending", TongTien = 500 };
+            var dp3 = new DatPhong { MaDatPhong = 3, MaPhong = "P103", TrangThai = "Pending", TongTien = 500 };
             var dc3 = new DatCoc { MaDatCoc = 3, MaDatPhong = 3, NgayDatCoc = new DateTime(nam, thang, 20) };
 
             // Đơn thành công ngoài kỳ
-            var dp4 = new DatPhong { MaDatPhong = 4, TrangThai = "Success", TongTien = 9999 };
+            var dp4 = new DatPhong { MaDatPhong = 4, MaPhong = "P104", TrangThai = "Success", TongTien = 9999 };
             var dc4 = new DatCoc { MaDatCoc = 4, MaDatPhong = 4, NgayDatCoc = new DateTime(nam, thang + 1, 1) };
 
+            context.Phongs.AddRange(phong1, phong2, phong3, phong4);
             context.DatPhongs.AddRange(dp1, dp2, dp3, dp4);
             context.DatCocs.AddRange(dc1, dc2, dc3, dc4);
             await context.SaveChangesAsync();
@@ -141,7 +147,9 @@ namespace lxrhotel.API.Tests
         {
             // Arrange
             var context = CreateContext();
-            var don = new DatPhong { MaDatPhong = 1, TrangThai = "Pending", TongTien = 1200 };
+            var phong = new Phong { MaPhong = "P101", LoaiPhong = "Standard", MaKs = "KS01", TrangThai = "Trống" };
+            var don = new DatPhong { MaDatPhong = 1, MaPhong = "P101", TrangThai = "Pending", TongTien = 1200 };
+            context.Phongs.Add(phong);
             context.DatPhongs.Add(don);
             await context.SaveChangesAsync();
 
@@ -161,7 +169,9 @@ namespace lxrhotel.API.Tests
         {
             // Arrange
             var context = CreateContext();
-            var don = new DatPhong { MaDatPhong = 1, TrangThai = "Pending", TongTien = 1200 };
+            var phong = new Phong { MaPhong = "P101", LoaiPhong = "Standard", MaKs = "KS01", TrangThai = "Trống" };
+            var don = new DatPhong { MaDatPhong = 1, MaPhong = "P101", TrangThai = "Pending", TongTien = 1200 };
+            context.Phongs.Add(phong);
             context.DatPhongs.Add(don);
             await context.SaveChangesAsync();
 
@@ -182,7 +192,9 @@ namespace lxrhotel.API.Tests
         {
             // Arrange
             var context = CreateContext();
-            var don = new DatPhong { MaDatPhong = 1, TrangThai = "Pending" };
+            var phong = new Phong { MaPhong = "P101", LoaiPhong = "Standard", MaKs = "KS01", TrangThai = "Trống" };
+            var don = new DatPhong { MaDatPhong = 1, MaPhong = "P101", TrangThai = "Pending" };
+            context.Phongs.Add(phong);
             context.DatPhongs.Add(don);
             await context.SaveChangesAsync();
 
@@ -208,7 +220,7 @@ namespace lxrhotel.API.Tests
         {
             // Arrange
             var context = CreateContext();
-            var khachHang = new KhachHang { MaKh = 1, TrangThai = initialStatus };
+            var khachHang = new KhachHang { MaKh = 1, TrangThai = initialStatus, Email = "test@example.com", HoTen = "Test User", MatKhau = "password" };
             context.KhachHangs.Add(khachHang);
             await context.SaveChangesAsync();
 
